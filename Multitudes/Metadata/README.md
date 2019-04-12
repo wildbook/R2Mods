503a302d2c7650b377af1c575be39fcfa6b0b5e3
wildbook <book.wille@gmail.com>
2019-04-12 15:51:31 +0200

    Initial code commit

---
+++ Multitudes/Metadata/README.md
@@ -0,0 +1,18 @@
+Have you ever thought "Damn, this game doesn't give me enough things to kill", or "Would be nice if I had a few more friends so I could steal their loot"?  
+Ever been stuck wondering "How would the game scale if I got 150 players ingame?"
+
+**Congratulations. This mod is for you.**
+
+----
+
+By faking a playercount higher than your actual playercount, this mod allows you to multiply your loot by any amount.  
+It consists of like three lines of code, and all you need to know is that it most likely works.
+
+To set a custom multiplier, use `mod_wb_set_multiplier X` in the ingame console.  
+`mod_wb_set_multiplier 4` is default and makes the game scale loot and enemies as if you were playing with four times as many players as you are.
+
+Setting a very high multiplier will make the game choke for a few minutes before you get ingame. Highest tested so far was 2 players and x250, effectively scaling as 500 players. It took around 5 minutes to generate a map, and once we were in there was both loot and enemies *everywhere*.
+
+----
+
+Credit for this mod's name and icon goes entirely to [Sipondo](https://thunderstore.io/package/Sipondo/), check out their mods as well.
---
+++ Multitudes/Multitudes.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using MonoMod.Utils;
+using RoR2;
+using UnityEngine;
+
+namespace Multitudes
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("dev.wildbook.multitudes", "Multitudes", "1.0")]
+    public class Multitudes : BaseUnityPlugin
+    {
+        public static int Multiplier = 4;
+
+        public void Awake()
+        {
+            // Needed to register our commands as well when the game registers its own
+            IL.RoR2.Console.Awake += il =>
+            {
+                var c = new ILCursor(il);
+                c.GotoNext(x => x.MatchStloc(1));
+                c.EmitDelegate<Func<Type[], Type[]>>(orig => orig.Concat(GetType().Assembly.GetTypes()).ToArray());
+            };
+
+            IL.RoR2.Run.FixedUpdate += il =>
+            {
+                var c = new ILCursor(il);
+                c.GotoNext(x => x.MatchCallvirt<Run>("set_livingPlayerCount"));
+                c.EmitDelegate<Func<int, int>>(x => x * Multiplier);
+
+                c.GotoNext(x => x.MatchCallvirt<Run>("set_participatingPlayerCount"));
+                c.EmitDelegate<Func<int, int>>(x => x * Multiplier);
+            };
+        }
+
+        // Random example command to set multiplier with
+        [ConCommand(commandName = "mod_wb_set_multiplier", flags = ConVarFlags.None, helpText = "Lets you pretend to have more friends than you actually do.")]
+        private static void CCSetMultiplier(ConCommandArgs args)
+        {
+            if (args.Count != 1 || !int.TryParse(args[0], out Multiplier))
+                Debug.Log("Invalid arguments.");
+            else
+                Debug.Log($"Multiplier set to {Multiplier}. Good luck!");
+        }
+
+        // Random example command to set multiplier with
+        [ConCommand(commandName = "mod_wb_get_multiplier", flags = ConVarFlags.None, helpText = "Lets you know how what Multitudes' multiplier is set to.")]
+        private static void CCGetMultiplier(ConCommandArgs args)
+        {
+            Debug.Log($"Your multiplier is currently {Multiplier}. Good luck!");
+        }
+    }
+}
