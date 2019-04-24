2b54f7346ed4f9fbf0f93058b91d692328cee3f7
wildbook <book.wille@gmail.com>
2019-04-25 01:58:47 +0200

    [Utilities] Add GetMethodInfo

---
+++ Utilities/MethodHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reflection;
+
+namespace MiniRpcLib.Extensions
+{
+    public static class MethodHelper
+    {
+        public  static MethodInfo GetMethodInfo<T>(this T a) where T : Delegate => a.Method;
+    }
+}
